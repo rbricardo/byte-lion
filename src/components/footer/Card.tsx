f9a2c92e7aeed88a5acf16561977b062940dc7e6
Ricardo Barbosa <rb.ricardo01@gmail.com>
2021-04-24 12:47:14 -0300

    feat: add card component

---
+++ src/components/footer/Card.tsx
@@ -0,0 +1,25 @@
+import React from 'react'
+
+interface Props {
+  description: string
+  imgPath: string
+  title: string
+}
+
+const Card: React.FC<Props> = ({ description, imgPath, title }) => {
+  return (
+    <div className="flex flex-col items-center justify-center h-80 w-64 shadow-lg bg-white">
+      <div className="w-10">
+        <img src={imgPath} />
+      </div>
+      <div className="mt-8 mb-2">
+        <p className="text-sm font-medium">{title}</p>
+      </div>
+      <div className="w-2/3">
+        <p className="text-xs">{description}</p>
+      </div>
+    </div>
+  )
+}
+
+export default Card
---
+++ src/helpers/mocks/cardsInfos.ts
@@ -0,0 +1,23 @@
+export const cardsInfos = [
+  {
+    id: 1,
+    imgPath: 'images/pane-1-icon.png',
+    title: 'Blue',
+    description:
+      'Blue Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras ac diam enim. Nulla orci eros, tempor eget turpis eget, porttitor porta mauris. Pellentesque at orci tempor.'
+  },
+  {
+    id: 2,
+    imgPath: 'images/pane-2-icon.png',
+    title: 'Green',
+    description:
+      'Green Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras ac diam enim. Nulla orci eros, tempor eget turpis eget, porttitor porta mauris. Pellentesque at orci tempor.'
+  },
+  {
+    id: 3,
+    imgPath: 'images/pane-3-icon.png',
+    title: 'Red',
+    description:
+      'Red Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras ac diam enim. Nulla orci eros, tempor eget turpis eget, porttitor porta mauris. Pellentesque at orci tempor.'
+  }
+]
