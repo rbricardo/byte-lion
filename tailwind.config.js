c771a4fc4e85ac2c7139647ba206c5994175d2ed
Ricardo Barbosa <rb.ricardo01@gmail.com>
2021-04-25 18:12:19 -0300

    fix: tailwind file

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,8 @@
 
 module.exports = {
   purge: [
-    './pages/**/*.{js,ts,jsx,tsx}',
-    './components/**/*.{js,ts,jsx,tsx}',
-    ''
+    '.src/pages/**/*.{js,ts,jsx,tsx}',
+    './src/components/**/*.{js,ts,jsx,tsx}'
     ],
   theme: {
     variants: {
